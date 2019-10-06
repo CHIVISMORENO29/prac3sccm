e795491fd0622266be12fb6054d9596495861564
Silvia del Carmen Cardona Moreno <55223689+CARMENMORENO19@users.noreply.github.com>
2019-10-06 00:22:30 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/prac3sccm/MainActivity.java
@@ -0,0 +1,30 @@
+package mx.edu.tesoem.isc.prac3sccm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent principal=new Intent(MainActivity.this,frm1_activity.class);
+                startActivity(principal);
+                finish();
+
+            }
+        },4000);
+    }
+}
+
+
+
+
